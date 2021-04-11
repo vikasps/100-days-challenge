899d5a23c51cd178c4ed2a0b6610243d24716e14
unknown <vikaspsmech18@gmail.com>
2021-04-11 16:18:55 +0530

    index.html

---
+++ 1-color-percent-loader/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("bt_one").addEventListener("click" , function(){
+    var ip1  = document.getElementById("inputData").value;
+    document.getElementById("roller").style.width = ip1 +"%";
+});
