802a69aaaceec986fd999d68cb05db3127d04fff
unknown <vikaspsmech18@gmail.com>
2021-04-21 10:57:42 +0530

    index.html

---
+++ 6_ taking input  fora tag/script.js
@@ -0,0 +1,6 @@
+document.getElementById("submit").addEventListener("click",function(){
+var mysearch = document.getElementById("search").value;
+    document.getElementById("output").setAttribute("href", mysearch);
+
+});
+
